6266b7b08afcde9778fa6ed43ee0e1cd938a82c3
neuecc <ils@neue.cc>
2021-11-21 00:58:41 +0900

    only double quote

--- src/Blog2/Program.cs
+++ src/Blog2/Program.cs
@@ -118,8 +118,8 @@ GitHub:<a href=""https://github.com/neuecc/"">neuecc</a>
 
 // Create footer
 var footer = @"<ul>
-<li>Index: <a href=\""https://neue.cc\"">neue.cc</a><li>
-<li>Powered by: <a href=\""https://github.com/neuecc/Blog2\"">https://github.com/neuecc/Blog2</a>
+<li>Index: <a href=""https://neue.cc"">neue.cc</a><li>
+<li>Powered by: <a href=""https://github.com/neuecc/Blog2"">https://github.com/neuecc/Blog2</a>
 </ul>";
 
 // Generate Root Index
