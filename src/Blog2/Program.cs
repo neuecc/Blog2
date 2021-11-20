f28adbdb54e4d96b10f7fb7efc093e2fe41ec1b6
neuecc <ils@neue.cc>
2021-11-20 22:18:22 +0900

    program code

---
+++ src/Blog2/Program.cs
@@ -0,0 +1,223 @@
+﻿using Markdig;
+using Markdig.Renderers;
+using Markdig.Renderers.Html;
+using Markdig.Syntax;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+var dir = args[0]; // output dir
+
+static string BuildHtml(string title, string content, string side, string footer)
+{
+    return @$"<!DOCTYPE html>
+<html dir=""ltr"" lang=""ja"">
+<head>
+    <meta charset=""utf-8"" />
+    <title>{title}</title>
+    <link rel=""shortcut icon"" href=""http://neue.cc/favicon.ico"" />
+	<link rel=""stylesheet"" href=""http://neue.cc/wp-content/themes/neuecc/style.css"" type=""text/css"" media=""screen"" />
+    <link href=""https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/themes/prism.min.css"" rel=""stylesheet"" />
+</head>
+<body>
+	<script src=""https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/components/prism-core.min.js""></script>
+    <script src=""https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/plugins/autoloader/prism-autoloader.min.js""></script>
+    <script src=""https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/plugins/normalize-whitespace/prism-normalize-whitespace.min.js""></script>
+    <div id=""wrapper"">
+        <div id=""header""></div>
+        <div id=""content"">{content}</div>
+        <div id=""side"">{side}</div>
+        <div id=""footer"">{footer}</div>
+    </div>
+</body>
+";
+}
+
+// Load Files and create body.
+var artciles = Directory.EnumerateFiles(dir)
+    .AsParallel()
+    .Select(x =>
+    {
+        var fileName = Path.GetFileNameWithoutExtension(x);
+
+        var match = Regex.Match(fileName, @"(\d{4})-(\d{2})-(\d+)(.+)");
+        var yyyy = match.Groups[1].Value;
+        var mm = match.Groups[2].Value;
+        var dd = match.Groups[3].Value;
+        var dd_no = dd + match.Groups[4].Value;
+
+        if (yyyy == "" || mm == "" || dd_no == "")
+        {
+            throw new Exception($"File name is not matched yyyy-MM-dd*, {Path.GetFileName(x)}");
+        }
+
+        var (first, others) = ReadAllTextWithoutFirstLine(x);
+        if (!first.StartsWith("#"))
+        {
+            throw new Exception($".md file does not contains #title, {Path.GetFileName(x)}");
+        }
+
+        var title = first.Trim('#', ' ');
+        var bodyTitle = $"<h1><a href=\"http://neue.cc/{yyyy}/{mm}{dd_no}.html\">{title}</a></h1>";
+        var bodyDate = $"<ul class=\"date\"><li>{yyyy}-{mm}-{dd}</li></ul>";
+        var body = "<div class=\"entry_body\">" + Markdown.ToHtml(others) + "</div>";
+
+        return new Article(
+            Url: new PageUrl(yyyy, mm, dd_no),
+            Body: bodyTitle + Environment.NewLine + bodyDate + Environment.NewLine + body,
+            Title: title
+        );
+    })
+    .WhereNotNull()
+    .ToArray();
+
+// Create Side
+var sideArchives = artciles
+    .GroupBy(x => (x.Url.yyyy, x.Url.mm))
+    .OrderByDescending(x => x.Key)
+    .Select(x => $"<li><a href=\"http://neue.cc/{x.Key.yyyy}/{x.Key.mm}/\">{x.Key.yyyy}-{x.Key.mm}</a>");
+
+var side = $@"
+<h3>Profile</h3>
+<div class=""side_body"" align=""center"">
+<b>Yoshifumi Kawai</b><br />
+<br />
+<a href=""https://cysharp.co.jp/"">Cysharp, Inc</a><br />
+CEO/CTO<br />
+<br />
+Microsoft MVP for Developer Technologies(C#)<br />
+April 2011<br />
+|<br />
+July 2022<br />
+<br />
+Twitter:<a href=""https://twitter.com/neuecc/"">@neuecc</a>
+GitHub:<a href=""https://github.com/neuecc/"">neuecc</a>
+</div>
+
+<h3>Archive</h3>
+<div class=""side_body"">
+<ul>
+{string.Join(Environment.NewLine, sideArchives)}
+</ul>
+</div>
+";
+
+// Create footer
+var footer = "<ul><li>Index: <a href=\"http://neue.cc\">neue.cc</a><li></ul>";
+
+// Generate Root Index
+var rootDir = "";
+await GenerateIndexWithPagingAsync(artciles.OrderByDescending(x => x), rootDir, null);
+
+// Generate YYYY/index.html
+await Parallel.ForEachAsync(artciles.GroupBy(x => x.Url.yyyy), async (yyyy, _) =>
+{
+    // item.OrderBy(x => x.OriginalFileName);
+    var yyyyPath = CreateDirectory(rootDir, yyyy.Key);
+    await GenerateIndexWithPagingAsync(yyyy.OrderByDescending(x => x), yyyyPath, $"{yyyy.Key}");
+
+    // Generate mm/index.html
+    foreach (var mm in yyyy.GroupBy(x => x.Url.mm))
+    {
+        var mmmmPath = CreateDirectory(yyyyPath, mm.Key);
+        await GenerateIndexWithPagingAsync(mm.OrderByDescending(x => x), mmmmPath, $"{yyyy.Key}-{mm.Key}");
+
+        // Generate single .html
+        foreach (var item in mm)
+        {
+            var html = BuildHtml("neue cc - " + item.Title, item.Body, side, footer);
+            Console.WriteLine($"Generating {item.Url.yyyy}/{item.Url.mm}/{item.Url.dd_no}.html");
+            await File.WriteAllTextAsync(Path.Combine(mmmmPath, item.Url.dd_no + ".html"), html);
+        }
+    }
+});
+
+/// <summary>Create and return combined path</summary>
+string CreateDirectory(string root, string path)
+{
+    var dir = Path.Combine(root, path);
+    if (!Directory.Exists(dir))
+    {
+        Directory.CreateDirectory(dir);
+    }
+    return dir;
+}
+
+(string FirstLine, string others) ReadAllTextWithoutFirstLine(string path)
+{
+    var lines = File.ReadLines(path);
+    string? first = null;
+    var others = new StringBuilder();
+    foreach (var line in lines)
+    {
+        if (first == null)
+        {
+            first = line;
+        }
+        else
+        {
+            others.AppendLine(line);
+        }
+    }
+    return (first!, others.ToString());
+}
+
+async Task GenerateIndexWithPagingAsync(IEnumerable<Article> source, string root, string? title)
+{
+    var page = 1;
+    var articles = source.Chunk(15).ToArray();
+    foreach (var items in articles)
+    {
+        var body = new StringBuilder();
+        foreach (var item in items)
+        {
+            body.AppendLine(item.Body);
+        }
+
+        // hasPrev
+        if (page != 1)
+        {
+            if ((page - 1) == 1)
+            {
+                body.AppendLine($"<a href=\"{root}/\">Prev |</a>");
+            }
+            else
+            {
+                body.AppendLine($"<a href=\"{root}/{page - 1}\">Prev |</a>");
+            }
+        }
+
+        // hasNext
+        if (page != articles.Length)
+        {
+            body.AppendLine($"<a href=\"{root}/{page + 1}\">| Next</a>");
+        }
+
+        var t = (title == null) ? "neue cc" : ("neue cc - " + title);
+        var html = BuildHtml(t, body.ToString(), side!, footer!);
+
+        var path = (page == 1) ? "index.html" : $"{page}.html";
+        var dir = root;
+        Console.WriteLine("Generating " + title + " Page " + page);
+        await File.WriteAllTextAsync(Path.Combine(dir, path), html);
+
+        page++;
+    }
+}
+
+public record Article(string Title, string Body, PageUrl Url) : IComparable<Article>
+{
+    public int CompareTo(Article? other)
+    {
+        return Comparer<(string, string, string)>.Default.Compare((Url.yyyy, Url.mm, Url.dd_no), (other.Url.yyyy, other.Url.mm, other.Url.dd_no));
+    }
+}
+public record PageUrl(string yyyy, string mm, string dd_no);
+public static class Extensions
+{
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> source)
+        where T : class
+    {
+        return source.Where(x => x != null)!;
+    }
+}
---
+++ tools/WordPressExporter/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+// load wp_posts dump of neue.cc
+var originalXml = XDocument.Load("wp_posts.xml");
+var mapped = originalXml.Descendants("wp_posts")
+    .Select(x => new wp_posts
+    {
+        ID = (ulong)x.Element("ID")!,
+        post_auhtor = (long)x.Element("post_author")!,
+        post_category = (long)x.Element("post_category")!,
+        post_date = (DateTime)x.Element("post_date")!,
+        post_date_gmt = (DateTime)x.Element("post_date_gmt")!,
+        post_content = (string)x.Element("post_content")!,
+        post_title = (string)x.Element("post_title")!,
+        post_status = (string)x.Element("post_status")!,
+        guid = (string)x.Element("guid")!,
+        post_type = (string)x.Element("post_type")!,
+    })
+    .Where(x => x is { post_status: "publish", post_type: "post" } && x.guid.EndsWith(".html"))
+    .OrderBy(x => x.post_date)
+    .Select(x =>
+    {
+        // "http://neue.cc/2007/10/22_13.html"
+        var match = Regex.Match(x.guid, @"(\d{4})/(\d{2})/(.+)\.html");
+        var yyyy = match.Groups[1].Value;
+        var mm = match.Groups[2].Value;
+        var dd_no = match.Groups[3].Value;
+
+        // <pre lang="..."></pre> -> ```... ```
+        var body = x.post_content;
+        body = body.Replace("</pre>", "```").Replace("<pre>", "```");
+        body = Regex.Replace(body, "<pre lang=\"(.+)\">", "```$1");
+
+        return new
+        {
+            Date = x.post_date,
+            Url = new
+            {
+                yyyy,
+                mm,
+                dd_no,
+            },
+            Title = x.post_title,
+            Body = body
+        };
+    })
+    .ToArray();
+
+// Output files
+
+var dir = "articles";
+if (!Directory.Exists(dir))
+{
+    Directory.CreateDirectory(dir);
+}
+
+Console.WriteLine("Output Files");
+foreach (var item in mapped)
+{
+    var fileName = $"{item.Url.yyyy}-{item.Url.mm}-{item.Url.dd_no}.md";
+    // # Title
+    // body
+
+    Console.WriteLine(fileName + " " + item.Title);
+    File.WriteAllText(Path.Combine(dir, fileName), "# " + item.Title + Environment.NewLine + Environment.NewLine + item.Body);
+}
+
+#pragma warning disable CS8618 // allow nul to nullable
+
+// table dump of WordPress posts db
+public class wp_posts
+{
+    public ulong ID { get; set; }
+    public long post_auhtor { get; set; }
+    public long post_category { get; set; }
+    public DateTime post_date { get; set; }
+    public DateTime post_date_gmt { get; set; }
+    public string post_content { get; set; }
+    public string post_title { get; set; }
+    public string post_status { get; set; } // enum('publish', 'draft', 'private', 'static', 'object', 'attachment', 'inherit', 'future', 'pending')
+    public string guid { get; set; }
+    public string post_type { get; set; }
+
+    public override string ToString()
+    {
+        return post_date.ToString("yyyy/MM/dd") + " " + post_title;
+    }
+}
+
+#pragma warning restore CS8618
